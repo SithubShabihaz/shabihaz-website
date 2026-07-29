4867a1df1af3c310977d938c918a9eb9755c05fa
SithubShabihaz <shabihazsite@gmail.com>
2026-07-29 16:48:21 +0500

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.mjs
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},  // ✅ Tailwind v4 ke liye
-  },
-};
-
-export default config;
