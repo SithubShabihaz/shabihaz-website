81145c76857284f79700b204f58c9797d6b366bc
SithubShabihaz <shabihazsite@gmail.com>
2026-08-01 13:58:03 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   // output: 'export',
   images: {
+    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         protocol: 'https',
