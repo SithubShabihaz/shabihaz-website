ced0e418a65fdbbba31be17570dab068c826ae2b
SithubShabihaz <shabihazsite@gmail.com>
2026-08-04 14:23:30 +0500

    Refactor imports and maintain component structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@ import AboutHeroSection from '@/components/about/AboutHeroSection';
 
 import dynamic from 'next/dynamic';
 const AboutTech = dynamic(() => import('@/components/about/AboutTech'));
-const AboutHeroSection = dynamic(() => import('@/components/about/AboutHeroSection'));
 const AboutTeamSection = dynamic(() => import('@/components/about/AboutTeamSection'));
 const AboutCard = dynamic(() => import('@/components/about/AboutCard'));
 const Footer = dynamic(() => import('@/components/Footer'));
