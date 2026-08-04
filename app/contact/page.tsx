ade0dcff6e5fc9a7277e0afda2dcdc0c3a8eade9
SithubShabihaz <shabihazsite@gmail.com>
2026-08-04 14:22:19 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import ContactHeroSection from '@/components/contact/ContactHeroSection';
-import ContactForm from '@/components/contact/ContactForm';
-import CalendlyContactSection from '@/components/contact/CalendlyContactSection';
-import ContactBottomSection from '@/components/contact/ContactBottomSection';
-import Footer from '@/components/Footer';
+
+import dynamic from 'next/dynamic';
+const ContactForm = dynamic(() => import('@/components/contact/ContactForm'));
+const CalendlyContactSection = dynamic(() => import('@/components/contact/CalendlyContactSection'));
+const ContactBottomSection = dynamic(() => import('@/components/contact/ContactBottomSection'));
+const Footer = dynamic(() => import('@/components/Footer'));
 
 export default function Contact() {
   return (
